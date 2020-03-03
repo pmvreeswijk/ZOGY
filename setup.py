7d26185f96b96e1f0fc233bd203b835ce297efc5
Paul Vreeswijk <pmvreeswijk@gmail.com>
2020-03-03 15:53:12 -0300

    changed python version requirement to >=2.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/pmvreeswijk/ZOGY',
     author='Paul Vreeswijk, Kerry Paterson',
     author_email='pmvreeswijk@gmail.com',
-    python_requires='>=3',
+    python_requires='>=2.7',
     license='MIT',
     packages=find_packages(),
     install_requires=['numpy', 'astropy', 'matplotlib', 'scipy', 'pyfftw',
