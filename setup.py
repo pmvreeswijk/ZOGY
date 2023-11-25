73299cdec9666446b7ba0d01ac87b2bd2228f904
Paul Vreeswijk <pmvreeswijk@gmail.com>
2023-11-25 20:42:45 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name='zogy',
-    version='1.1.0',
+    version='1.2.2',
     description='a Python implementation of proper image subtraction (ZOGY 2016, ApJ, 830, 27)',
     url='https://github.com/pmvreeswijk/ZOGY',
     author='Paul Vreeswijk, Kerry Paterson',
     author_email='pmvreeswijk@gmail.com',
-    python_requires='>=2.7',
+    python_requires='>=3.6',
     license='MIT',
     packages=find_packages(),
     install_requires=['numpy', 'astropy', 'matplotlib', 'scipy', 'pyfftw',
