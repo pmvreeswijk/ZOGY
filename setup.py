c31fdbf9c6f0b3d4fd1165a1a78085936ddd45d2
Paul Vreeswijk <pmvreeswijk@gmail.com>
2024-03-05 18:46:49 +0100

    using latest version of astropy (aplpy package - now removed from blackbox - was preventing this)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='zogy',
-    version='1.2.2',
+    version='1.3.0',
     description='a Python implementation of proper image subtraction (ZOGY 2016, ApJ, 830, 27)',
     url='https://github.com/pmvreeswijk/ZOGY',
     author='Paul Vreeswijk, Kerry Paterson',
@@ -9,7 +9,7 @@ setup(
     python_requires='>=3.6',
     license='MIT',
     packages=find_packages(),
-    install_requires=['numpy', 'astropy==5.3.4', 'matplotlib', 'scipy',
+    install_requires=['numpy', 'astropy', 'matplotlib', 'scipy',
                       'pyfftw', 'lmfit', 'sip_tpv', 'scikit-image', 'healpy',
                       'photutils', 'tensorflow==2.13', 'fitsio',
                       'fsspec', 'gcsfs']
