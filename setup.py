2a1c3e24eaf7920eadaff2a1045e9fc6e872da75
Paul Vreeswijk <pmvreeswijk@gmail.com>
2024-09-05 16:40:57 +0200

    setup.py: trying out tensorflow 2.16

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     packages=find_packages(),
     install_requires=['numpy', 'astropy', 'matplotlib', 'scipy', 'psutil',
                       'pyfftw', 'lmfit', 'sip_tpv', 'scikit-image', 'healpy',
-                      'photutils', 'tensorflow==2.13', 'fitsio', 'fsspec', 'gcsfs']
+                      'photutils', 'tensorflow==2.16', 'fitsio', 'fsspec', 'gcsfs']
 )
